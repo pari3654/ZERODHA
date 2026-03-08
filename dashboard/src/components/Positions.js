6a6ae13d6e42d1636ffda385c0a2716508410be6
PARI PATEL <paripatel3654@gmail.com>
2026-03-08 17:03:16 +0530

    Update API endpoint to production URL

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -7,7 +7,7 @@ const Positions = () => {
 
   useEffect(() => {
 
-    axios.get("http://localhost:8080/allPositions",{
+    axios.get("https://zerodha-2rqq.onrender.com/allPositions",{
        withCredentials: true
     })
     .then((res)=>{
